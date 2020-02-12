a6c6560fc4c05130ff5c813b7b361d023e5fa28c
Josue Aparicio <josue9aparicio@gmail.com>
2020-02-12 14:13:07 -0600

    Creacion del metodo desviacion estandar y tipica

--- Programación 2 estadistica/estadistica.cs
+++ Programación 2 estadistica/estadistica.cs
@@ -12,13 +12,26 @@ namespace Programación_2_estadistica
 
         public double media(String[] serie)
         {
-            foreach (String valor in serie)
+            foreach (string valor in serie)
             {
                 suma += int.Parse(valor);
             }
             return suma / serie.Length;
-
-
+        }
+        public double estandar(String[] serie)
+        {
+            double media_aritmetica = media(serie),
+                suma = 0,
+                estandar = 0;
+            foreach (string valor in serie)
+            {
+                suma += Math.Pow(double.Parse(valor) - media_aritmetica, 2);
+            }
+            return suma / serie.Length;
+        }
+        public double tipica(String[] serie)
+        {
+            return Math.Sqrt(estandar(serie));
         }
     }
 }
